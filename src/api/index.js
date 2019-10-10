baa878dd869abee74ad50479a9ffa47405238857
yujl <1012380521@qq.com>
2019-10-10 14:58:01 +0800

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 /* eslint-disable no-console */
-export const api = process.env.NODE_ENV === 'development' ? 'http://192.168.10.150:8055/v1/api/' : 'http://192.168.10.150:8055/v1/api/';
+export const api = process.env.NODE_ENV === 'development' ? 'http://192.168.10.150:8055/v1/api/' : 'http://faxian.chehaiyang.com/v1/api/';
